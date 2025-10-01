f37617b21f5880f7a2a3fbda8f4412aab8f050de
1Envy2 <adyatmay.f@gmail.com>
2025-10-02 00:04:35 +0700

    materi 4

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Kelas;
+use App\Models\UserModel;
+
+class UserController extends Controller
+{
+    public $userModel;
+    public $kelasModel;
+    public function __construct()
+    {
+        $this -> userModel = new UserModel();
+        $this -> kelasModel = new kelas();
+        
+    }
+
+    public function store(Request $request){
+        $this ->userModel ->create([
+            'nama' => $request -> input('nama'),
+            'NPM' => $request -> input('npm'),
+            'kelas_id' => $request -> input('kelas_id'),
+        ]);
+        return redirect() -> to('/user');
+    }
+
+    public function create(){
+        $kelasModel = new kelas();
+        $kelas = $kelasModel->getkelas();
+        $data =[
+            'title' => 'Create User',
+            'kelas' => $kelas,
+
+        ];
+        return view('create_user', $data);
+    }
+
+    public function index(){
+        $data = [
+            'title' => 'List User',
+            'users' => $this -> userModel -> getUser(),
+        ];
+        return view('list_user', $data);
+    }
+}
+    
